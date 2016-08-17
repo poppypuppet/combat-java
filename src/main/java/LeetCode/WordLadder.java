77682ba6ad24a7c74e7659d7370ba15bccf12777
Victoria Zhang <weizhang@yahoo-inc.com>
2016-08-17 00:40:14 -0700

    WordLadder

--- src/main/java/LeetCode/WordLadder.java
+++ src/main/java/LeetCode/WordLadder.java
@@ -99,16 +99,14 @@ public class WordLadder {
                 for (int i = 0; i < array.length; i++) {
                     char c = array[i];
                     for (char replace = 'a'; replace <= 'z'; replace++) {
-                        if (replace != c) {
-                            array[i] = replace;
-                            String replacedWord = new String(array);
-                            if (tail.contains(replacedWord)) {
-                                return ans + 1;
-                            }
-                            if (!deleted.contains(replacedWord) && wordList.contains(replacedWord)) {
-                                nextHead.add(replacedWord);
-                                deleted.add(replacedWord);
-                            }
+                        array[i] = replace;
+                        String replacedWord = new String(array);
+                        if (tail.contains(replacedWord)) {
+                            return ans + 1;
+                        }
+                        if (!deleted.contains(replacedWord) && wordList.contains(replacedWord)) {
+                            nextHead.add(replacedWord);
+                            deleted.add(replacedWord);
                         }
                     }
                     array[i] = c;
@@ -116,6 +114,51 @@ public class WordLadder {
             }
             head = nextHead;
             ans++;
+        }
+
+        return 0;
+    }
+
+    public int ladderLength_sample(String beginWord, String endWord, Set<String> wordList) {
+        Set<String> beginSet = new HashSet<String>(), endSet = new HashSet<String>();
+
+        int len = 1;
+        HashSet<String> visited = new HashSet<String>();
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> set = beginSet;
+                beginSet = endSet;
+                endSet = set;
+            }
+
+            Set<String> temp = new HashSet<String>();
+            for (String word : beginSet) {
+                char[] chs = word.toCharArray();
+
+                for (int i = 0; i < chs.length; i++) {
+                    char old = chs[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chs[i] = c;
+                        String target = String.valueOf(chs);
+
+                        if (endSet.contains(target)) {
+                            return len + 1;
+                        }
+
+                        if (!visited.contains(target) && wordList.contains(target)) {
+                            temp.add(target);
+                            visited.add(target);
+                        }
+                    }
+                    chs[i] = old;
+                }
+            }
+
+            beginSet = temp;
+            len++;
         }
 
         return 0;
