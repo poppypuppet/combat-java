b6ba498f43fe4c5a4484874197b2afb68a03f2d8
Victoria Zhang <weizhang@yahoo-inc.com>
2016-08-18 08:32:28 -0700

    ReverseLinkedList

---
+++ src/main/java/LeetCode/ReverseLinkedList.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+import Interface.ListNode;
+
+public class ReverseLinkedList {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+}
