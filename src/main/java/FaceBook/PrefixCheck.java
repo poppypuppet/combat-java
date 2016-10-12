330039018d9a1051e48a97d0826760df56c5713e
Victoria Zhang <weizhang@yahoo-inc.com>
2016-10-12 16:52:34 -0700

    prefix - find

--- src/main/java/FaceBook/PrefixCheck.java
+++ src/main/java/FaceBook/PrefixCheck.java
@@ -1,14 +1,13 @@
 package FaceBook;
 
+import java.util.LinkedList;
 import java.util.List;
 
-public abstract class PrefixCheck {
+public class PrefixCheck {
     public Integer[] offsetX = {0, -1, 0, 1};
     public Integer[] offsetY = {-1, 0, 1, 0};
 
-    public void findPrefix(Character[][] board, StringBuilder candidate, List<String> ans, Integer x, Integer y) {
-        int m = board.length;
-        int n = board[0].length;
+    public void findPrefix(Character[][] board, StringBuilder candidate, List<String> ans, Integer x, Integer y, Integer m, Integer n) {
         if (x >= 0 && x < m && y >= 0 && y < n) {
             char c = board[x][y];
             if (c != '#') {
@@ -22,7 +21,7 @@ public abstract class PrefixCheck {
 
                 board[x][y] = '#';
                 for (int i = 0; i < 4; i++) {
-                    findPrefix(board, candidate, ans, x + offsetX[i], y + offsetY[i]);
+                    findPrefix(board, candidate, ans, x + offsetX[i], y + offsetY[i], , );
                 }
                 board[x][y] = c;
                 candidate.setLength(candidate.length() - 1);
@@ -30,5 +29,21 @@ public abstract class PrefixCheck {
         }
     }
 
-    public abstract Boolean isPrefix(String s);
+    public void main(String[] argv) {
+        PrefixCheck pfc = new PrefixCheck();
+        Character[][] board = {{}};
+        int m = board.length;
+        int n = board[0].length;
+        LinkedList<String> ans = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pfc.findPrefix(board, new StringBuilder(), ans, i, j, m, n);
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public Boolean isPrefix(String s) {
+        return true;
+    }
 }
