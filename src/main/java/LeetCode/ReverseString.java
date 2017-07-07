646e52ad6ace07f2e2c3fc3fccca0faf1ce2f760
weiwzhan <weiwzhan@microsoft.com>
2017-07-06 19:46:00 -0700

    reverse string

---
+++ src/main/java/LeetCode/ReverseString.java
@@ -0,0 +1,9 @@
+package LeetCode;
+
+/**
+ * Write a function that takes a string as input and returns the string reversed.
+ * Example:
+ * Given s = "hello", return "olleh".
+ */
+public class ReverseString {
+}
