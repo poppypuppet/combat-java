c2a91c48edc518145179081aedc15ad304bc6b39
weiwzhan <weiwzhan@microsoft.com>
2017-07-07 14:33:25 -0700

    reverse string

--- src/main/java/LeetCode/ReverseString.java
+++ src/main/java/LeetCode/ReverseString.java
@@ -6,4 +6,22 @@ package LeetCode;
  * Given s = "hello", return "olleh".
  */
 public class ReverseString {
+    public String reverseString(String s) {
+        char[] arr = s.toCharArray();
+        int start = 0, end = arr.length-1;
+        if (end > 0) {
+            while (start < end) {
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
+        }
+        return new String(arr);
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char c = arr[i];
+        arr[i] = arr[j];
+        arr[j] = c;
+    }
 }
